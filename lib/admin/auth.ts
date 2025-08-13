b3edffd421969a3c77fc88db23f80dfed543b172
SYED NABIL <89401427+SNFASA@users.noreply.github.com>
2025-08-14 01:44:56 +0800

    Delete lib/admin directory

--- lib/admin/auth.ts
+++
@@ -1,9 +0,0 @@
-import bcrypt from 'bcryptjs';
-
-export async function hashPassword(password: string) {
-  return bcrypt.hash(password, 10);
-}
-
-export async function comparePassword(raw: string, hash: string) {
-  return bcrypt.compare(raw, hash);
-}
